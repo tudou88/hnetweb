93ecb7ef692f6b3141202a31bce3c8a4054e83f2
SY <99464244+ivwv@users.noreply.github.com>
2024-04-24 10:10:49 +0800

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -1,7 +1,7 @@
 /**
  * Just replace BAREUR
  */ 
-const BAREURL = 'https://hnetweb.nbzdwss78.tk'
+const BAREURL = 'https://hideip.ivwv.site'
 
 /**
  * Non-developers, please modify without authorization, 98% report errors!
